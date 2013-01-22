c0f05597b3b7481e21c74245edeb054db2bc576d
Gabe Pike <gpike@isecpartners.com>
2013-01-22 02:06:46 -0800

    pushing current progress - needs lots of work

--- pyxie.py
+++ pyxie.py
@@ -11,12 +11,12 @@ Example:
 $ python
 >>> from intercept import *
 
-# Create a Intercept object on port 8081 (default)
->>> server = Intercept(port=8081) 
+# Create a Pyxie object on port 8081 (default)
+>>> server = Pyxie(port=8081) 
 
 # Set the mode, either manual or auto.
 # Manual makes the server look for the /needle/ in the packet, saves the
-# data in <Intercept instance>.data, pauses the server, and hands off
+# data in <Pyxie instance>.data, pauses the server, and hands off
 # control to the user (you).
 >>> server.manual('foobar')
 # Start the server
@@ -52,79 +52,125 @@ $ python
 
 """
 
-import socket, sys, time
+import socket, sys, time, struct
 from threading import Thread
 
-class Intercept:
+class Pyxie:
   NOMODE = 0
   AUTO = 1
   MANUAL = 2
 
-  def __init__(self, port=8081):
+  # TODO: Taken from mallory. Replace with own code or give credit in release.
+  def getrealdest(self, csock):   
+      """
+      This method only supports linux 2.4+. 
+      
+      Cross platform support coming soon.
+      """ 
+      try:
+          socket.SO_ORIGINAL_DST
+      except AttributeError:
+          # This is not a defined socket option
+          socket.SO_ORIGINAL_DST = 80
+          
+      # Use the Linux specific socket option to query NetFilter
+      odestdata = csock.getsockopt(socket.SOL_IP, socket.SO_ORIGINAL_DST, 16)
+      
+      # Unpack the first 6 bytes, which hold the destination data needed                                
+      _, port, a1, a2, a3, a4 = struct.unpack("!HHBBBBxxxxxxxx", odestdata)
+      address = "%d.%d.%d.%d" % (a1, a2, a3, a4)
+      
+      return address, port
+
+
+  def __init__(self, port=20755):
     self.port = port
-    self.mode = Intercept.NOMODE
+    self.mode = Pyxie.NOMODE
+    self.logfile = "pyxie.log"
+    self.csockections = []
+    self.r = self.replace
+    self.ra = self.replaceAll
   
   def auto(self, callback):
-    self.mode = Intercept.AUTO
+    self.mode = Pyxie.AUTO
     self.callback = callback
 
   def manual(self, needle="foobar"):
-    self.mode = Intercept.MANUAL
+    self.mode = Pyxie.MANUAL
     self.needle = needle
 
   def nomode(self):
-    self.mode = Intercept.NOMODE
+    self.mode = Pyxie.NOMODE
 
   # start the server
   def start(self):
-    if (self.mode == Intercept.NOMODE):
+    if (self.mode == Pyxie.NOMODE):
       print '[*] No interception mode is set (data will not be modified).'
       print '[*] Set mode with auto(callback) or manual(needle).'
     print '[+] Starting server'
 
-    self.thr = Thread(target=self.run)
+    self.logfd = open(self.logfile, "a")
+    self.startTime = time.time()
+    self.thr = Thread(target=self.handleConnection)
     self.thr.start()
 
   # stop the server
   def stop(self):
     self.running = False
-    self.conn.close()
-    self.sock.close()
+    try:
+      self.csock.close()
+      self.ssock.close()
+      self.dsock.close()
+      self.logfd.close()
+    except:
+      pass
 
   # run the server
-  def run(self):
+  def handleConnection(self):
     self.running = True
-    self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    self.sock.bind(("127.0.0.1", self.port))
-    self.sock.listen(1)
+    self.ssock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    self.ssock.bind(('', self.port))
+    self.ssock.listen(1)
+    self.dsock = None
 
-    while self.running:
+# TODO: move main loop to another method. spawn a thread for each connection.
+# don't close connection until it is closed by the source or destination
+    while self.running == True:
       try:
-        self.conn, self.addr = self.sock.accept()
+        self.csock, self.addr = self.ssock.accept()
+
+        daddr, dport = self.getrealdest(self.csock)
+        print "destination = %s:%s" % (daddr, dport)
+        
+        try:
+          self.dsock = socket.connect((daddr, dport))
+        except:
+          self.stop()
+          return
 
         # FIXME
-        self.data = self.conn.recv(1000000)
+        self.data = self.csock.recv(65535)
         self.modified = ""
 
-        print "%s [recv]: %s" % (time.time(), self.data)
+        self.logfd.write("[+%.2fs recv]: %s\n" % (time.time() - self.startTime, self.data))
 
-        if self.mode == Intercept.MANUAL:
+        if self.mode == Pyxie.MANUAL:
           if self.data.find(self.needle) != -1:
             print '[+] intercepted data: call unpause(modified) to continue'
             self.pause()
-        elif self.mode == Intercept.AUTO:
+        elif self.mode == Pyxie.AUTO:
           self.modified = self.callback(self.data)
 
-        self.conn.send(self.modified if self.modified != ""  else self.data)
-        self.conn.close()
+        self.dsock.send(self.modified if self.modified != "" else self.data)
+
+# TODO: receive response from server and forward to the client
+        self.csock.close()
       except Exception as e:
         print '[-] exception'
         print e.message
-        if self.conn:
-          self.conn.close()
         self.stop()
 
-    self.sock.close()
+    self.stop()
     print '[-] server terminated'
 
   def pause(self):
@@ -135,9 +181,15 @@ class Intercept:
   def unpause(self, modified=""):
     self.modified = modified
     self.paused = False
-  
+
+  def replace(self, needle, replacement):
+    self.unpause(self.data.replace(needle, replacement))
+
+  def replaceAll(self, replacement):
+    self.unpause(replacement)
+
 def main():
-  server = Intercept()
+  server = Pyxie()
   server.auto(foobalicious)
   server.start()
 
