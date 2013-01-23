23235d435ab750ff77beb87584183a3a5d0dc076
Gabe Pike <gpike@isecpartners.com>
2013-01-23 11:48:49 -0800

    finished bootstrapping tansparent proxy

--- pyxie.py
+++ pyxie.py
@@ -1,66 +1,9 @@
 #!/usr/bin/evn python
 
-"""
-*** intercept.py *** 
-
-This script should be used in conjunction with the hooked version of
-pidgin. It should be invoked from the python interactive interpreter.
-
-Example:
-
-$ python
->>> from intercept import *
-
-# Create a Pyxie object on port 8081 (default)
->>> server = Pyxie(port=8081) 
-
-# Set the mode, either manual or auto.
-# Manual makes the server look for the /needle/ in the packet, saves the
-# data in <Pyxie instance>.data, pauses the server, and hands off
-# control to the user (you).
->>> server.manual('foobar')
-# Start the server
->>> server.start()
-
-# lots of data output to console
-# ...
-# <server intercepts a packet with 'foobar' in it
-[+] intercepted data: call unpause(modified) to continue
-
-# Pass unpause() the modified data you want to send.
->>> server.unpause(server.data.replace('foobar', 'barfoo'))
-
-# Change the mode to 'auto' just for fun. But first, create a function
-# that modifies traffic.
-
->>> def foobalicious(data):
-...   return data.replace('foobar', 'barfoo')
-...
-
-# Now change the mode to auto. All traffic will be passed to
-# foobalicious(), which returns the modified data back to the caller.
->>> server.auto(foobalicious)
-
-# more data in console
-# ...
-
-# turn off interception
->>> server.nomode()
-
-# Stop the server when we're done.
->>> server.stop()
-
-"""
-
-import socket, sys, time, struct
-from threading import Thread
+import socket, sys, time, struct, traceback, re, threading
 
 class Pyxie:
-  NOMODE = 0
-  AUTO = 1
-  MANUAL = 2
-
-  # TODO: Taken from mallory. Replace with own code or give credit in release.
+    # TODO: Taken from mallory. Replace with own code or give credit in release.
   def getrealdest(self, csock):   
       """
       This method only supports linux 2.4+. 
@@ -82,119 +25,196 @@ class Pyxie:
       
       return address, port
 
-
   def __init__(self, port=20755):
     self.port = port
-    self.mode = Pyxie.NOMODE
     self.logfile = "pyxie.log"
-    self.csockections = []
-    self.r = self.replace
-    self.ra = self.replaceAll
+    self.connections = []
   
-  def auto(self, callback):
-    self.mode = Pyxie.AUTO
-    self.callback = callback
-
-  def manual(self, needle="foobar"):
-    self.mode = Pyxie.MANUAL
-    self.needle = needle
-
-  def nomode(self):
-    self.mode = Pyxie.NOMODE
-
   # start the server
   def start(self):
-    if (self.mode == Pyxie.NOMODE):
+    """
+    if (self.mode == TcpTransport.NOMODE):
       print '[*] No interception mode is set (data will not be modified).'
       print '[*] Set mode with auto(callback) or manual(needle).'
-    print '[+] Starting server'
-
+    """
+    
+    Pyxie.startTime = time.time()
     self.logfd = open(self.logfile, "a")
     self.startTime = time.time()
-    self.thr = Thread(target=self.handleConnection)
-    self.thr.start()
+    threading.Thread(target=self.acceptConnections).start()
 
   # stop the server
   def stop(self):
     self.running = False
     try:
-      self.csock.close()
-      self.ssock.close()
-      self.dsock.close()
+      self.psock.close()
       self.logfd.close()
+      for conn in self.connections:
+        conn.stop()
     except:
       pass
+    
+    print '[-] stopped server'
 
-  # run the server
-  def handleConnection(self):
+    # run the server
+  def acceptConnections(self):
+    print '[+] Starting server'
     self.running = True
-    self.ssock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    self.ssock.bind(('', self.port))
-    self.ssock.listen(1)
-    self.dsock = None
+    self.psock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    self.psock.bind(('', self.port))
+    self.psock.listen(1)
 
-# TODO: move main loop to another method. spawn a thread for each connection.
-# don't close connection until it is closed by the source or destination
     while self.running == True:
       try:
-        self.csock, self.addr = self.ssock.accept()
-
-        daddr, dport = self.getrealdest(self.csock)
+        print '[*] Waiting for connections...'
+        ssock, saddr = self.psock.accept()
+        daddr, dport = self.getrealdest(ssock)
+        dsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         print "destination = %s:%s" % (daddr, dport)
         
         try:
-          self.dsock = socket.connect((daddr, dport))
-        except:
-          self.stop()
-          return
+          dsock.connect((daddr, dport))
+          conn = TcpTransport(ssock, dsock)
+          self.connections.append(conn)
+          self.running = True
+          threading.Thread(target=conn.forward, args=(TcpTransport.S2D,)).start()
+          threading.Thread(target=conn.forward, args=(TcpTransport.D2S,)).start()
+        except Exception as e:
+          print "[-] %s" % e
+          dsock.close()
+          continue
 
-        # FIXME
-        self.data = self.csock.recv(65535)
-        self.modified = ""
+      except KeyboardInterrupt:
+        print 'got a keyboard interrupt!'
+        self.stop()
+        sys.exit(0)
+      except Exception as e:
+        print "[-] %s" % e
+        pass
+        
+    self.stop()
 
-        self.logfd.write("[+%.2fs recv]: %s\n" % (time.time() - self.startTime, self.data))
+  
+class TcpTransport:
+  NOMODE = 0
+  AUTO = 1
+  MANUAL = 2
 
-        if self.mode == Pyxie.MANUAL:
-          if self.data.find(self.needle) != -1:
+  S2D = 1
+  D2S = -1
+
+  def __init__(self, ssock, dsock):
+    self.ssock = ssock
+    self.dsock = dsock
+    self.startTime = time.time()
+    self.mode = TcpTransport.NOMODE
+    #self.r = self.replace
+    #self.ra = self.replaceAll
+
+  def rawAscii(self, data):
+    return re.sub(r'\s', ' ', re.sub(r'[^ -~]', r'.', data))
+  
+  def log(self, message):
+    #self.logfd.write("[+%.2fs recv]: %s\n" % (time.time() - self.startTime, data))
+    return "[+%.2fs src]: %s\n" % (time.time() - Pyxie.startTime, message)
+
+  def forward(self, *args):
+    direction = args[0]
+
+    ssock = self.ssock
+    dsock = self.dsock
+
+    if direction == TcpTransport.D2S:
+      ssock = self.dsock
+      dsock = self.ssock
+
+    data = ' '
+    while data:
+      try:
+        data = ssock.recv(8192)
+        if not data:
+          try:
+            ssock.shutdown(socket.SHUT_RD)
+            dsock.shutdown(socket.SHUT_WR)
+            self.running = False
+          finally:
+            return
+        self.modified = data
+
+        self.log(data)
+        print self.rawAscii(data)
+
+        if self.mode == TcpTransport.MANUAL:
+          if data.find(self.needle) != -1:
             print '[+] intercepted data: call unpause(modified) to continue'
             self.pause()
-        elif self.mode == Pyxie.AUTO:
-          self.modified = self.callback(self.data)
+        elif self.mode == TcpTransport.AUTO:
+          self.modified = self.callback(data)
 
-        self.dsock.send(self.modified if self.modified != "" else self.data)
-
-# TODO: receive response from server and forward to the client
-        self.csock.close()
-      except Exception as e:
-        print '[-] exception'
-        print e.message
+        try:
+          dsock.sendall(self.modified)
+        except:
+          self.running = False
+                
+      except KeyboardInterrupt:
+        print 'keyboard interrupt'
         self.stop()
+        return
 
-    self.stop()
-    print '[-] server terminated'
+      except Exception as e:
+        print "[-] %s" % e
+        traceback.print_exc()
+        return
+
+  """
+  def auto(self, callback):
+    self.mode = TcpTransport.AUTO
+    self.callback = callback
+
+  def manual(self, needle="foobar"):
+    self.mode = TcpTransport.MANUAL
+    self.needle = needle
+
+  def nomode(self):
+    self.mode = TcpTransport.NOMODE
 
   def pause(self):
-    self.paused = True
-    while self.paused:
-      pass
+      self.paused = True
+      while self.paused:
+        pass
 
   def unpause(self, modified=""):
     self.modified = modified
     self.paused = False
 
   def replace(self, needle, replacement):
-    self.unpause(self.data.replace(needle, replacement))
+    self.unpause(data.replace(needle, replacement))
 
   def replaceAll(self, replacement):
     self.unpause(replacement)
+  """
+
+  def stop(self):
+    self.running = False
+    try:
+      self.ssock.shutdown(socket.SHUT_RDWR)
+      self.dsock.shutdown(socket.SHUT_RDWR)
+      self.ssock.close()
+      self.dsock.close()
+    except:
+      pass
+    
+    print "Stopped %s" % self
 
 def main():
   server = Pyxie()
-  server.auto(foobalicious)
+  #server.auto(foobalicious)
   server.start()
 
+"""
 def foobalicious(data):
   return data.replace("foobar", "barfoo")
+"""
 
 if __name__ == "__main__":
   sys.exit(main())
