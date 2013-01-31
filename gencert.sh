8f767c2e053be05c0e29d6d68e88b63f6bcf0bd7
Gabe Pike <gpike@isecpartners.com>
2013-01-30 17:46:42 -0800

    modified gencert script to handle shell metacharacters

--- gencert.sh
+++ gencert.sh
@@ -13,11 +13,11 @@ if [ -z "$subj" ]; then
   subj="/CN=$domain/O=$domain, Inc./"
 fi
 
-if [ ! -e $newcerts ]; then
+if [ ! -e "$newcerts" ]; then
   mkdir -p $newcerts
 fi
 
-if [ -e $newcerts/$domain.pem ]; then
+if [ -e "$newcerts/$domain.pem" ]; then
   exit
 fi
 
@@ -31,7 +31,7 @@ if [ ! -e $base/ca.key ]; then
   openssl req -config openssl.cnf -new -x509 -extensions v3_ca -keyout $base/ca.key -out $base/ca.crt -days 3650 -nodes -subj '/CN=justtrustme.bro/O=Pyxie Trusted Packet Proxyz, Inc./C=US/ST=CA/L=San Francisco/emailAddress=gabe@isecpartners.com/'
 fi
 
-openssl req -new -nodes -outform PEM -keyout $newcerts/$domain.key -out $newcerts/$domain.csr -days 3650 -subj "$subj"
-openssl ca -startdate 130101120000Z -enddate 330101000000Z -md sha1 -config $config -policy policy_anything -notext -batch -out $newcerts/$domain.crt -infiles $newcerts/$domain.csr 
-cat $newcerts/$domain.key $newcerts/$domain.crt $base/ca.crt >> $newcerts/$domain.pem
-rm $newcerts/$domain.csr $newcerts/$domain.key $newcerts/$domain.crt
+openssl req -new -nodes -outform PEM -keyout "$newcerts/$domain.key" -out "$newcerts/$domain.csr" -days 3650 -subj "$subj"
+openssl ca -startdate 130101120000Z -enddate 330101000000Z -md sha1 -config $config -policy policy_anything -notext -batch -out "$newcerts/$domain.crt" -infiles "$newcerts/$domain.csr"
+cat "$newcerts/$domain.key" "$newcerts/$domain.crt" "$base/ca.crt" >> "$newcerts/$domain.pem"
+rm "$newcerts/$domain.csr" "$newcerts/$domain.key" "$newcerts/$domain.crt"
